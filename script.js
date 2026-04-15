9d5fbaa89bde8a077dd030b6fa90bbb565074a6f
luzmariavasquezmartinez6-max <luzmariavasquezmartinez6@gmail.com>
2026-04-15 13:53:58 -0600

    222

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function mostrarmensaje(){
-  let mensaje=document.get1ementById(mensaje);
-  
+  let mensaje=document.getElementById("mensaje");
+  mensaje.innerHTML= "cuida y protege a los animales";
+  mensaje.style.display = "block";
+
+  setTimeout( ()=> { 
+    mensaje.style.opacity = "1";
+  mensaje.style.transform ="translateY(0)";
+  },50 );
+
 }
