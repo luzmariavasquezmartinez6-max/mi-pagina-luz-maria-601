37d76e281d79f82f0db156856107cbd088271b33
luzmariavasquezmartinez6-max <luzmariavasquezmartinez6@gmail.com>
2026-04-27 07:29:20 -0600

    222

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function mostrarmensaje(){
 }
 function mostrarmensaje2(){
   let mensaje=document.getElementById("mensaje2");
-  mensaje.innerHTML= "La mejor huella para el corazón, son las patitas de un perro";
+  mensaje.innerHTML= "hola mi nombre es luz";
   mensaje.style.display = "block";
 
   setTimeout( ()=> { 
