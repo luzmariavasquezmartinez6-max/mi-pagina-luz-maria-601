ef5cfb19de223a3457b32ab7e2c075f99cd76383
luzmariavasquezmartinez6-max <luzmariavasquezmartinez6@gmail.com>
2026-05-06 13:40:00 -0600

    cambio9909

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-function mostrarmensaje(){
-  let mensaje=document.getElementById("mensaje");
-  mensaje.innerHTML= "cuida y protege a los animales";
-  mensaje.style.display = "block";
-
-  setTimeout( ()=> { 
-    mensaje.style.opacity = "1";
-  mensaje.style.transform ="translateY(0)";
-  },50 );
-
+function mostrarmensaje() {
+    const mensaje = document.getElementById("mensaje");
+    mensaje.innerText = "¡Gracias por interesarte en el proyecto!";
+    mensaje.classList.add("mostrar");
 }
-function mostrarmensaje2(){
-  let mensaje=document.getElementById("mensaje2");
-  mensaje.innerHTML= "hola mi nombre es luz";
-  mensaje.style.display = "block";
 
-  setTimeout( ()=> { 
-    mensaje.style.opacity = "1";
-  mensaje.style.transform ="translateY(0)";
-  },50 );
-
+function mostrarmensaje2() {
+    const mensaje = document.getElementById("mensaje2");
+    mensaje.innerText = "¡Hola Luz! Bienvenida a tu página web ✨";
+    mensaje.classList.add("mostrar");
 }
