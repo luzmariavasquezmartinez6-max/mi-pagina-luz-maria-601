00018c674c7752994b928ce11cc10180eb8d013f
luzmariavasquezmartinez6-max <luzmariavasquezmartinez6@gmail.com>
2026-05-12 13:27:24 -0700

    CAMIO 9

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-
 function mostrarmensaje() {
+
     const mensaje = document.getElementById("mensaje");
-    mensaje.innerText = "¡Gracias por interesarte en el proyecto!";
+
+    mensaje.innerText =
+        "¡Gracias por interesarte en el proyecto!";
+
     mensaje.classList.add("mostrar");
 }
 
 function mostrarmensaje2() {
+
     const mensaje = document.getElementById("mensaje2");
-    mensaje.innerText = "¡Hola Luz! Bienvenida a tu página web ✨";
+
+    mensaje.innerText =
+        "¡Hola Luz! Bienvenida a tu página web ✨";
+
     mensaje.classList.add("mostrar");
 }
+
+/* BOTÓN MODO OSCURO */
+
 const boton = document.getElementById("toggleDark");
 
 boton.addEventListener("click", () => {
+
     document.body.classList.toggle("dark-mode");
 
-    // Cambiar texto del botón
     if (document.body.classList.contains("dark-mode")) {
+
         boton.innerText = "☀️ Modo claro";
+
     } else {
+
         boton.innerText = "🌙 Modo oscuro";
     }
 });
-
-
