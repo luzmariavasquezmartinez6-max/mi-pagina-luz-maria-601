4a15937dfda5c595e835dfd0692a32bccb3d7a33
luzmariavasquezmartinez6-max <luzmariavasquezmartinez6@gmail.com>
2026-05-12 10:36:04 -0700

    fyufffgjig

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function mostrarmensaje() {
+ mostrarmensaje() {
     const mensaje = document.getElementById("mensaje");
     mensaje.innerText = "¡Gracias por interesarte en el proyecto!";
     mensaje.classList.add("mostrar");
 }
-
+function
 function mostrarmensaje2() {
     const mensaje = document.getElementById("mensaje2");
     mensaje.innerText = "¡Hola Luz! Bienvenida a tu página web ✨";
@@ -20,4 +20,21 @@ boton.addEventListener("click", () => {
     } else {
         boton.innerText = "🌙 Modo oscuro";
     }
+});
+// ACORDEÓN
+
+const acordeonBtns = document.querySelectorAll(".acordeon-btn");
+
+acordeonBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+
+        const contenido = btn.nextElementSibling;
+
+        if (contenido.style.display === "block") {
+            contenido.style.display = "none";
+        } else {
+            contenido.style.display = "block";
+        }
+
+    });
 });
